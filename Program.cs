5c059ba603fc85dc64eab515c722ab4d876643ff
Khudaat E. Mammadli <khudaatem@code.local>
2024-10-20 12:55:42 +0400

    add task

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace TASK
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region task1
+            int[] numbers = { 37, 25, 75, 43, 87, 91, 53, 62, 15 };
+            int max = numbers[0];
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] > max)
+                {
+                    max = numbers[i];
+                }
+            }
+            Console.WriteLine("the large number: " + max);
+            #endregion
+        }
+    }
+}
