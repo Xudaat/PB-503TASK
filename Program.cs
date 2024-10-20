134460e1f0e3f0883f9fa5669b3f3396793e90ce
Khudaat E. Mammadli <khudaatem@code.local>
2024-10-20 14:44:48 +0400

    added TASK2

--- Program.cs
+++ Program.cs
@@ -17,6 +17,30 @@
             }
             Console.WriteLine("the large number: " + max);
             #endregion
+            #region Task2
+            var product1 = new { id = 1, name = "product1", price = 10, StockCount = 100 };
+            var product2 = new { id = 2, name = "product2", price = 20, StockCount = 200 };
+            var product3 = new { id = 3, name = "product3", price = 30, StockCount = 300 };
+            var product4 = new { id = 4, name = "product4", price = 40, StockCount = 400 };
+            var product5 = new { id = 5, name = "product5", price = 50, StockCount = 500 };
+
+            var products = new[] { product1, product2, product3, product4, product5 };
+            int sum = 0;
+            int count = 0;
+            for (int i = 0; i < products.Length; i++)
+            {
+                if (products[i].id % 2 == 1)
+                {
+                    sum += products[i].price;
+                    count++;
+
+                }
+            }
+            int avg = sum / count;
+            Console.WriteLine(avg);
+            #endregion
+
         }
+
     }
 }
